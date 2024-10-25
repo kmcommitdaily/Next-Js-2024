2492e1b73ecd56aa77e645c43d62f51d9f9625e1
kim <kmgalos0909@gmail.com>
2024-10-25 08:28:27 +0800

    update

---
+++ app/users/page.tsx
@@ -0,0 +1,40 @@
+import { link } from 'fs';
+import React from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    next: { revalidate: 10 },
+  }); // nextjs will store the result in data cache based on file system
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>NAME:</th>
+            <th>EMAIL:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;
